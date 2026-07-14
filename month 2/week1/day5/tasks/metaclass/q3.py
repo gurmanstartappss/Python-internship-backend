3c643838ca11f16919ebdb27cdb2abdc01870537
Gurman <garggurman@gmail.com>
2026-07-14 18:52:42 +0530

    Merge origin/main into main

--- month 2/week1/day5/tasks/metaclass/q3.py
+++ month 2/week1/day5/tasks/metaclass/q3.py
@@ -1,14 +1,8 @@
 class gh(type):
     def __new__(cls,nn,bb,ss):
-        count=0
-        for key in ss:
-            if not key.startswith("__") :
-                count+=1
-        ss["attribute_count"]=count
-        
+        ss["created_by"]= "admin"
         return super().__new__(cls,nn,bb,ss)
 class yo(metaclass=gh):
-    def show(name,class):
-        return 
+    pass
 
-print(yo.attribute_count)       
+print(yo.created_by)       
